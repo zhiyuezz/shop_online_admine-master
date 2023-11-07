faeabd2b05f5d3d84d06d4fb32d3b9115449e725
WindBlack <m15951316382@163.com>
2023-11-07 10:37:16 +0800

    vue初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,9 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justiy-center h-100 items-center">
+    <div class="w-10 h-10 bg-pink-300"></div>
+  </div>
 </template>
 
 <style lang="scss">
