4dcbdf86f5f1e86eda7e5e3bba8a22597ca7a05f
WindBlack <m15951316382@163.com>
2023-11-07 11:17:21 +0800

    modified:   src/pages.json 	modified:   src/pages/login/login.vue 	modified:   src/pages/my/my.vue 	modified:   vite.config.ts

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -11,7 +11,6 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
-
     <uni-section title="设置" type="line">
       <uni-list>
         <uni-list-item title="去登录" show-arrow :to="`/pages/login/login`"> </uni-list-item>
