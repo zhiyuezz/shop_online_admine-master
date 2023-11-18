46d32a06a09d88e182b56e9f5812559883cfbffb
WindBlack <2474429669@qq.com>
2023-11-18 10:45:13 +0800

    首页

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,13 +1,13 @@
 import 'vue'
-import wGuess from '@/components/wGuess/wGuess.vue'
+import Guess from '@/components/Guess/Guess.vue'
 import wSwiper from '@/components/wSwiper/wSwiper.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
     wSwiper: typeof wSwiper
-    wGuess: typeof wGuess
+    Guess: typeof Guess
   }
 }
 
 // 组件实例类型
-export type wGuessInstance = InstanceType<typeof wGuess>
+export type GuessInstance = InstanceType<typeof Guess>
