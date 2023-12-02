09b3badca66138369495a8a056a5f5e09feeb27c
zhiyuezz <3032149617@qq.com>
2023-12-02 19:33:37 +0800

    创建t_user表实例/分页查询用户列表/导出用户信息

--- src/stores/modules/address.ts
+++
@@ -1,9 +0,0 @@
-export const useAddressStore = defineStore('address', () => {
-  const selectedAddress = ref<AddressItem>()
-
-  const changeSelectedAddress = (val: AddressItem) => {
-    selectedAddress.value = val
-  }
-
-  return { selectedAddress, changeSelectedAddress }
-})
